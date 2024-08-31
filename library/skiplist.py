b023b394aa03113c3939792a5342a2db389ec808
apoorva <apoorva.me1@gmail.com>
2024-08-31 14:45:13 +0530

    testing skiplist failed

---
+++ library/skiplist.py
@@ -0,0 +1,2 @@
+class SkipList:
+    pass
---
+++ tests/test_skiplist.py
@@ -0,0 +1,14 @@
+import pytest
+from library.skiplist import SkipList
+
+def test_skiplist_insert_and_search():
+    skip_list = SkipList(max_level=4)
+    skip_list.insert(1, "Book 1")
+    assert skip_list.search(1) == "Book 1"
+    assert skip_list.search(2) is None
+
+def test_skiplist_delete():
+    skip_list = SkipList(max_level=4)
+    skip_list.insert(1, "Book 1")
+    skip_list.delete(1)
+    assert skip_list.search(1) is None
