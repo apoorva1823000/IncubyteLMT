06e8849b2036c77ea95e344e96136c43d9235e6a
apoorva <apoorva.me1@gmail.com>
2024-08-31 14:46:16 +0530

    Added a feature in skiplist.py and performed testing

--- library/skiplist.py
+++ library/skiplist.py
@@ -1,2 +1,43 @@
+import random
+
+class Node:
+    def __init__(self, key, value, level):
+        self.key = key
+        self.value = value
+        self.forward = [None] * (level + 1)
+
 class SkipList:
-    pass
+    def __init__(self, max_level):
+        self.max_level = max_level
+        self.header = Node(None, None, max_level)
+        self.current_level = 0
+
+    def random_level(self):
+        level = 0
+        while random.random() < 0.5 and level < self.max_level:
+            level += 1
+        return level
+
+    def insert(self, key, value):
+        update = [None] * (self.max_level + 1)
+        current = self.header
+
+        for i in range(self.current_level, -1, -1):
+            while current.forward[i] and current.forward[i].key < key:
+                current = current.forward[i]
+            update[i] = current
+
+        current = current.forward[0]
+
+        if current is None or current.key != key:
+            new_level = self.random_level()
+            if new_level > self.current_level:
+                for i in range(self.current_level + 1, new_level + 1):
+                    update[i] = self.header
+                self.current_level = new_level
+
+            new_node = Node(key, value, new_level)
+            for i in range(new_level + 1):
+                new_node.forward[i] = update[i].forward[i]
+                update[i].forward[i] = new_node
+
